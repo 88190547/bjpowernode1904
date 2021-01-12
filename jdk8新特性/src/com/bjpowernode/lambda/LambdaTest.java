90795eaf7c3dd737938e71a9256e9e87e06e5d2c
88190547 <88190547@qq.com>
2021-01-13 00:09:41 +0800

    增加自测试jar包和lambda测试类

---
+++ jdk8新特性/src/com/bjpowernode/lambda/LambdaTest.java
@@ -0,0 +1,21 @@
+package com.bjpowernode.lambda;
+
+import org.junit.Test;
+
+public class LambdaTest {
+
+    @Test
+    public void test01() {
+
+        Runnable r1 = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("我爱北京天安门");
+            }
+        };
+        r1.run();
+        System.out.println("******************");
+        Runnable r2 = () -> System.out.println("我爱北京故宫");
+        r2.run();
+    }
+}
